4c7d3a05a9db076bb20c948de06272a804f43bca
minh15092003 <100700718+TinyTots-ePj@users.noreply.github.com>
2022-06-22 21:32:19 +0700

    t

---
+++ Program02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intVal;
+            double dbVal;
+            string strVal;
+            intVal = 10;
+            dbVal = 3.142;
+            strVal = "FPT Aptech";
+            Console.WriteLine("{0} is an integer value", intVal);
+            Console.WriteLine("{0} is an double value", dbVal);
+            Console.WriteLine("{0} is an string", strVal);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program04.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReferenceType refer = new ReferenceType();
+            refer.valueVal = 5;
+            Test(refer);
+            Console.WriteLine("teh value of the variable is {0}", refer.valueVal);
+            Console.ReadLine();
+        }
+        static void Test(ReferenceType refer)
+        {
+            int temp = 5;
+            refer.valueVal = temp * 2;
+        }
+    }
+}
